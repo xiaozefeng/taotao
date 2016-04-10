0d7b88c816c4806744247b427838a51fb17074ae
zfxiao <zfxiao@ewaytec.cn>
2016-04-10 13:45:56 +0800

    添加注释

--- taotao-manage/taotao-manage-web/src/main/java/com/taotao/controller/ItemCatController.java
+++ taotao-manage/taotao-manage-web/src/main/java/com/taotao/controller/ItemCatController.java
@@ -10,7 +10,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.taotao.common.pojo.EUTreeNode;
 import com.taotao.service.ItemCatService;
-
+/**
+ * 商品分类
+ * <p>Title: ItemCatController</p>
+ * <p>Description: </p>
+ * <p>Company: www.ewaytec.cn</p> 
+ * @author	肖泽锋
+ * @date	2016年4月10日下午1:45:09
+ * @version 1.0
+ */
 @Controller
 @RequestMapping("/item/cat")
 public class ItemCatController {
